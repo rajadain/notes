33fe16c516c40f417dcd57162963227d68f48f95
Terence Tuhinanshu <terence@tuhinanshu.com>
2014-07-24 20:51:21 -0400

    Add Notes factory to standardize CRUD operations
    Also introduce the `id` field in a note

--- js/app.js
+++ js/app.js
@@ -2,6 +2,70 @@
 
 angular.module('notes-app', ['ngRoute', 'ngStorage'])
 
+  .factory('Notes', function($localStorage) {
+    var notes = $localStorage.$default({
+      notes: [
+        {
+          id      : 0,
+          title   : "Sample Note",
+          content : "This is some sample content for this sample note.",
+          created : new Date(1404010593 * 1000),
+          modified: new Date(1404010731 * 1000),
+          liked   : false,
+        }
+      ]
+    }).notes;
+
+    var nextId = notes.length || 1;
+    var NoteService = {};
+
+    NoteService.get = function(id) {
+      for (var i = 0; i < notes.length; i++) {
+        if (notes[i].id == id) return notes[i];
+      }
+      return null;
+    };
+
+    NoteService.all = function() {
+      return notes;
+    }
+
+    NoteService.new = function() {
+      var thisId = nextId++;
+      return {
+        id      : thisId,
+        title   : "",
+        content : "",
+        created : new Date(),
+        modified: new Date(),
+        liked   : false,
+      };
+    };
+
+    NoteService.add = function(note) {
+      if (NoteService.get(note.id)) {
+        // Note already exists
+        return false;
+      } else {
+        notes.push(note);
+        return true;
+      }
+    };
+
+    NoteService.del = function(id) {
+      for (var i = 0; i < notes.length; i++) {
+        if (notes[i].id == id) {
+          var deletedNote = notes[i];
+          notes.splice(i, 1);
+          return deletedNote;
+        }
+      }
+      return null;
+    };
+
+    return NoteService;
+  })
+
   .config(function($routeProvider) {
     $routeProvider
       .when('/all', {
