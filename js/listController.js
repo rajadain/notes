ee40bdb535a88246690e1ca126d1b7210e83c5f3
Terence Tuhinanshu <terence@tuhinanshu.com>
2014-07-31 20:02:49 -0400

    Switch List Controller and View to use ui-router

--- js/listController.js
+++ js/listController.js
@@ -3,21 +3,23 @@
     .module('notes-app')
     .controller('List', List);
 
-  function List(noteService, $location) {
+  function List(noteService, $stateParams) {
     var vm = this;
-    vm.isFav = ($location.absUrl().indexOf("fav") > -1);
 
     vm.notes = getNotes();
 
     vm.remove = remove;
 
     function getNotes() {
-      if (vm.isFav) {
-        return noteService.all().filter(function(note) {
-          return note.liked;
-        });
-      } else {
-        return noteService.all();
+      switch ($stateParams.listId) {
+        case "all":
+          return noteService.all();
+          break;
+        case "fav":
+          return noteService.all().filter(function(note) { return note.liked; });
+          break;
+        default:
+          return null;
       }
     }
 
